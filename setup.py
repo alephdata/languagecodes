4ccef7af8b2fe75dd335e27254c87f6d81d17c45
Friedrich Lindenberg <friedrich@pudo.org>
2018-06-24 11:56:18 +0200

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='languagecodes',
-    version='1.0.2',
+    version='1.0.3',
     description="A library that normalises language codes",
     classifiers=[
         "Development Status :: 3 - Alpha",
